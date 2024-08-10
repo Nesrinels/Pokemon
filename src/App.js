e4d91442b683b73e904699933041100d9d5c2aa9
Nesrinels <lounissi.nesrine@gmail.com>
2024-08-10 22:30:57 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import PokeDex from "./component/PokeDex";
+
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [loading, setLoading] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+         <div className="App">
+           {loading ? <h1>"LOADING"</h1> : <PokeDex pokemonList={pokemonList} />}
+         </div>
+
+     
   );
 }
 
